a7734ddbb51726397bf3ae19bd0c99d9f4bacce8
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-15 14:48:17 +0430

    ConfigModule added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule],
   controllers: [],
   providers: [],
 })
