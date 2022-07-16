cb1f16b128a7501c2d1726eb47c9fee1f50cd082
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-16 13:32:55 +0430

    CreatePostDto added.

---
+++ src/posts/dto/create-post.dto.ts
@@ -0,0 +1,36 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsNumber()
+  @IsOptional()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  pictures: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+}
