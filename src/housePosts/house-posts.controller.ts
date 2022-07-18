2655cd70ffe026293f7b30a865300a9312bcb9e5
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-18 20:56:44 +0430

    create new HousePost functionality added.

--- src/housePosts/house-posts.controller.ts
+++ src/housePosts/house-posts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { GetUser } from 'src/globals/decorators/get-user.decorator';
+import { CreateHousePostsDto } from './dto/create-house-post.dto';
+import { HousePostsService } from './house-posts.service';
 
-@Controller('/housePosts')
-export class HousePostsController {}
+@Controller('profile/housePosts')
+export class HousePostsController {
+    constructor(private housePostsService : HousePostsService){}
+
+    @Post('add')
+    @UseGuards(JwtAuthGuard)
+    createHousePost(@Body()createHousePostDto : CreateHousePostsDto , @GetUser()user : User){
+        return this.housePostsService.createHousePost(createHousePostDto , user)
+    }
+}
--- src/housePosts/house-posts.service.ts
+++ src/housePosts/house-posts.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateHousePostsDto } from './dto/create-house-post.dto';
 @Injectable()
 export class HousePostsService {
-    constructor(private prisma : PrismaService){
+  constructor(private prisma: PrismaService) {}
 
-    }
+  async createHousePost(dto : CreateHousePostsDto , user : User) {
+    const housePost = await this.prisma.post.create({
+        data: {
+          title: dto.title,
+          city: dto.city,
+          Description: dto.description,
+          pictures: dto.pictures,
+          price: dto.price,
+          tags: dto.tags,
+          housePost: {
+            create: {
+              area : dto.area,
+              hasElevator : dto.hasElevator,
+              hasParking : dto.hasParking,
+              numberOfFloor : dto.numberOfFloor,
+              numberOfRooms : dto.numberOfRooms,
+              numberOfTotalFloors : dto.numberOfTotalFloors,
+              yearOfBuild : dto.yearOfBuild,
+              pricePerMeter : dto.pricePerMeter
+            },
+          },
+          userID : user.id ,
+        },
+      });
+      const updatedAdmin = await this.prisma.admin.update({
+        where : {
+          username : 'admin1'
+        },
+        data : {
+          incomingRequests : {
+            connect : {
+              id : housePost.id
+            }
+          }
+        }
+    
+      })
+      return "house post sent to admin!"  
+  }
 }
