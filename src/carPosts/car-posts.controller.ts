a13efd50fc392e42925c61518d3fae03997e268f
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-18 17:39:09 +0430

    add new carPost end point and functionality added.

--- src/carPosts/car-posts.controller.ts
+++ src/carPosts/car-posts.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { GetUser } from 'src/globals/decorators/get-user.decorator';
+import { CarPostsService } from './car-posts.service';
+import { CreateCarPostsDto } from './dto/create-car-post.dto';
 
-@Controller('carPosts')
+@Controller('profile/carPosts')
 @UseGuards(JwtAuthGuard)
 export class CarPostsController {
-  @Post()
-  create() {}
+  constructor(private carPostsService: CarPostsService) {}
+  @Post('add')
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createCarPostDto: CreateCarPostsDto, @GetUser() user: User) {
+    return this.carPostsService.create(createCarPostDto , user);
+  }
 }
--- src/carPosts/car-posts.service.ts
+++ src/carPosts/car-posts.service.ts
@@ -1,6 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { debugPort } from 'process';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { runInThisContext } from 'vm';
+import { CreateCarPostsDto } from './dto/create-car-post.dto';
 
 @Injectable()
 export class CarPostsService {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
+  async  create(dto: CreateCarPostsDto, user: User) {
+    const post = await this.prisma.post.create({
+      data: {
+        title: dto.title,
+        city: dto.city,
+        Description: dto.description,
+        pictures: dto.pictures,
+        price: dto.price,
+        tags: dto.tags,
+        carPost: {
+          create: {
+            color: dto.color,
+            model: dto.model,
+            usage: dto.usage,
+            gearbox: dto.gearBox,
+            motorCondition: dto.motorCondition,
+          },
+        },
+        userID : user.id ,
+      },
+    });
+    const updatedAdmin = await this.prisma.admin.update({
+      where : {
+        username : 'admin1'
+      },
+      data : {
+        incomingRequests : {
+          connect : {
+            id : post.id
+          }
+        }
+      }
+  
+    })
+    return "car post sent to admin!"
+    
+  }
 }
