057b39522e995a9b68419ed05ee63c000e7f6f72
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-18 21:58:04 +0430

    users can get all posts without login or signup

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,27 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { PostsService } from "./posts.service";
 
 @Controller('posts')
 export class PostsController {
     constructor(private postsService : PostsService){}
-    
+
+    @Get()
+    getAllPosts(){
+        return this.postsService.getAllPosts()
+    }
+
+    @Get('carPosts')
+    getCarPosts(){
+        return this.postsService.getCarPosts()
+    }
+
+    @Get('housePosts')
+    getHousePosts(){
+        return this.postsService.getHousePosts()
+    }
+
+    @Get('furniturePosts')
+    getFurniturePosts(){
+        return this.postsService.getFurniturePosts()
+    }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,7 +1,61 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class PostsService {
-    constructor(private prisma : PostsService){}
+    constructor(private prisma : PrismaService){}
 
+    async getAllPosts(){
+        const allPosts = await this.prisma.post.findMany({
+            include : {
+                carPost:true,
+                housePost:true,
+                furniturePost:true,
+            },
+            where : {
+                isApproved : true
+              }
+        })
+        return allPosts;
+    }
+
+    async getCarPosts(){
+        const carPosts = await this.prisma.post.findMany({
+            include : {
+              carPost : true
+            },
+            where : {
+              isApproved : true,
+              housePost : null,
+              furniturePost : null
+            }
+          });
+          return carPosts;
+    }
+    async getFurniturePosts(){
+        const furniturePosts = await this.prisma.post.findMany({
+            include : {
+              furniturePost : true
+            },
+            where : {
+              isApproved : true,
+              housePost : null,
+              carPost : null
+            }
+          });
+        return furniturePosts;
+    }
+    async getHousePosts(){
+        const housePosts = await this.prisma.post.findMany({
+            include : {
+              housePost : true
+            },
+            where : {
+              isApproved : true,
+              carPost : null,
+              furniturePost : null
+            }
+          });
+        return housePosts;
+    }
 }
