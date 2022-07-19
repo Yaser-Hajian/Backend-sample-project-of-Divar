b4b1964a08fb54d3ce5b40496f8d7fb36433e096
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-19 10:51:31 +0430

    user can create furniture post and it will be sent to admin.

--- src/furniturePosts/furniture-posts.controller.ts
+++ src/furniturePosts/furniture-posts.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from "@nestjs/common";
-import { FurniturePostsService } from "./furniture-posts.service";
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { GetUser } from 'src/globals/decorators/get-user.decorator';
+import { CreateFurniturePostsDto } from './dto/create-furniture-post.dto';
+import { FurniturePostsService } from './furniture-posts.service';
 @Controller('profile/furniturePosts')
-export class FurniturePostsController{
-    constructor(private furniturePostsService : FurniturePostsService){}
-
-    
+export class FurniturePostsController {
+  constructor(private furniturePostsService: FurniturePostsService) {}
+  @Post('add')
+  @UseGuards(JwtAuthGuard)
+  createFurniturePosts(
+    @Body() createFurniturePostsDto: CreateFurniturePostsDto,
+    @GetUser() user: User,
+  ) {
+    return this.furniturePostsService.createFurniturePost(createFurniturePostsDto , user);
+  }
 }
