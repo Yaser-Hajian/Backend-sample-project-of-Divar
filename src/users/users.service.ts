ddce517ee977108dcf59f9d24cccc5be18c02101
Yaser Hajian <mohammadyaser1379@gmail.com>
2022-07-19 10:56:28 +0430

    FIX: a bug fixed whene user wants to get specific type of posts.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,55 +8,61 @@ export class UsersService {
 
   async getCarPosts(user: User) {
     const carPosts = await this.prisma.post.findMany({
-      include : {
-        carPost : true
+      include: {
+        carPost: true,
       },
-      where : {
-        userID : user.id ,
-        isApproved : true
-      }
+      where: {
+        userID: user.id,
+        isApproved: true,
+        housePost: null,
+        furniturePost: null,
+      },
     });
     return carPosts;
   }
 
   async getHousePosts(user: User) {
     const housePosts = await this.prisma.post.findMany({
-        include : {
-          housePost : true
-        },
-        where : {
-          userID : user.id,
-          isApproved : true
-        }
-      });
+      include: {
+        housePost: true,
+      },
+      where: {
+        userID: user.id,
+        isApproved: true,
+        carPost: null,
+        furniturePost: null,
+      },
+    });
     return housePosts;
   }
 
   async getFurniturePosts(user: User) {
     const furniturePosts = await this.prisma.post.findMany({
-        include : {
-          furniturePost : true
-        },
-        where : {
-          userID : user.id,
-          isApproved : true
-        }
-      });
+      include: {
+        furniturePost: true,
+      },
+      where: {
+        userID: user.id,
+        isApproved: true,
+        housePost: null,
+        carPost: null,
+      },
+    });
     return furniturePosts;
   }
 
   async getAllPosts(user: User) {
     const allPosts = await this.prisma.post.findMany({
-        include : {
-            carPost:true,
-            housePost:true,
-            furniturePost:true,
-        },
-        where : {
-            userID : user.id,
-            isApproved : true
-          }
-    })
+      include: {
+        carPost: true,
+        housePost: true,
+        furniturePost: true,
+      },
+      where: {
+        userID: user.id,
+        isApproved: true,
+      },
+    });
     return allPosts;
   }
 }
